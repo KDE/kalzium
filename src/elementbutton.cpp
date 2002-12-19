e114f9ba0844a369120c22474e1a52c7aa0eb5ff
Robert Gogolok <robertgogolok@gmx.de>
2002-12-19 10:59:33 +0000

    copyright text
    svn path=/trunk/kdeedu/kalzium/; revision=194818

--- src/elementbutton.cpp
+++ src/elementbutton.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+      
+      elementbutton.cpp  -  description
+                             -------------------
+    begin                : Die Dez  28 16:04:56 CET 2002
+    copyright            : (C) 2002 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #include "elementbutton.h"
 #include "elementbutton.moc"
 
--- src/elementbutton.h
+++ src/elementbutton.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+
+      elementbutton.h  -  description
+                             -------------------
+    begin                : Die Dez  28 15:59:34 CET 2002
+    copyright            : (C) 2002 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #ifndef ELEMENTBUTTON_H
 #define ELEMENTBUTTON_H
 
