93cd96a05b5419f9238ac6e047ac935cc4767551
Carsten Niehaus <cniehaus@gmx.de>
2003-04-04 11:25:58 +0000

    fix #56736
    CCMAIL: 56736-done@bugs.kde.org
    
    svn path=/trunk/kdeedu/kalzium/; revision=218256

--- src/kalzium.cpp
+++ src/kalzium.cpp
@@ -168,7 +168,7 @@ void Kalzium::setupAllElementKPButtons()
 
 		//Now we add the WhatsThis-help for each button
 		QWhatsThis::add( element[n], i18n("Click here to get information about %1.").arg(eleminfo.Symbol) );
-		QToolTip::add( element[n], eleminfo.Name );
+		QToolTip::add( element[n], eleminfo.Name.utf8() );
 	}
 
     //////////////////
