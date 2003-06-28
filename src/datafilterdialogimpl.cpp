c767243eb1a1426aa700cb69c05116097d2d1ff9
Carsten Niehaus <cniehaus@gmx.de>
2003-06-28 15:09:01 +0000

    hey! Cool script :) adding the missing licences
    svn path=/trunk/kdeedu/kalzium/; revision=234348

--- src/datafilterdialogimpl.cpp
+++ src/datafilterdialogimpl.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+
+        datafilterdialog.cpp  -  description
+                             -------------------
+    begin                : June 2003 
+    copyright            : (C) 2003 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "datafilterdialogimpl.h"
 
 #include <qcheckbox.h>
--- src/datafilterdialogimpl.h
+++ src/datafilterdialogimpl.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+
+        datafilterdialog.h  -  description
+                             -------------------
+    begin                : June 2003 
+    copyright            : (C) 2003 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef DATAFILTERDIALOGIMPL_H
 #define DATAFILTERDIALOGIMPL_H
 
