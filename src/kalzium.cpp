36beaa61c0c1d11395f21233b18d600d9ce2a7c5
Robert Gogolok <robertgogolok@gmx.de>
2003-09-26 11:58:55 +0000

    wizard icon for quiz
    CCMAIL: cniehaus@gmx.de
    
    svn path=/trunk/kdeedu/kalzium/; revision=254090

--- src/kalzium.cpp
+++ src/kalzium.cpp
@@ -675,7 +675,7 @@ void Kalzium::setupActions()
 	legendToggleAction->setChecked( main_config->readBoolEntry( "legend" ) );
 	//END LEGEND
 
-    (void) new KAction (i18n("Test Your &Knowledge"),0, this, SLOT(slotKnowledge()), actionCollection(), "test_your_knowledge");
+    (void) new KAction (i18n("Test Your &Knowledge"), "wizard", 0, this, SLOT(slotKnowledge()), actionCollection(), "test_your_knowledge");
     (void) new KAction (i18n("&Plot"),0, this, SLOT(slotPlotData()), actionCollection(), "startplotting");
     (void) new KAction (i18n("&Search Data"), "find", CTRL+Key_F, this, SLOT(slotSearchData()), actionCollection(), "searchdata");
     (void) new KAction (i18n("&Calculations"),0, this, SLOT(slotCalculations()), actionCollection(), "calculations");
