bddbf32be09442c11236525510c2d8a4a1e7543a
Carsten Niehaus <cniehaus@gmx.de>
2004-04-30 18:00:51 +0000

    adding copyrights (all gpl2)
    svn path=/branches/make_kdeedu_cool/kdeedu/kalzium/; revision=307601

--- src/kalzium.cpp
+++ src/kalzium.cpp
@@ -1,7 +1,15 @@
-/*
- * Copyright (C) 2003 Carsten Niehaus <cniehaus@kde.org>
- */
-
+/***************************************************************************
+    copyright            : (C) 2003, 2004 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "kalzium.h"
 #include "pse.h"
 #include "elementbutton.h"
--- src/kalzium.h
+++ src/kalzium.h
@@ -1,7 +1,15 @@
-/*
- * Copyright (C) 2003, 2004 Carsten Niehaus <cniehaus@kde.org>
- */
-
+/***************************************************************************
+    copyright            : (C) 2003, 2004 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef _KALZIUM_H_
 #define _KALZIUM_H_
 
