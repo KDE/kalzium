800dfb4389593ddac92cac42983c8b3289fda2ee
Anne-Marie Mahfouf <annma@kde.org>
2004-05-23 20:22:41 +0000

    add keybord shortcuts
    svn path=/trunk/kdeedu/kalzium/; revision=313915

--- src/kalzium.cpp
+++ src/kalzium.cpp
@@ -93,8 +93,8 @@ Kalzium::Kalzium()
 	/*
 	 * the misc actions
 	 **/
-	m_pTimelineAction = new KAction(i18n("Show Timeline"), 0, this, SLOT(slotShowTimeline()), actionCollection(), "use_timeline");
-	m_pPlotAction = new KAction(i18n("Plot Data"), 0, this, SLOT(slotPlotData()), actionCollection(), "plotdata");
+	m_pTimelineAction = new KAction(i18n("Show &Timeline"), 0, this, SLOT(slotShowTimeline()), actionCollection(), "use_timeline");
+	m_pPlotAction = new KAction(i18n("&Plot Data"), 0, this, SLOT(slotPlotData()), actionCollection(), "plotdata");
 	
 	/*
 	 * the standardactions
