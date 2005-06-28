d0fcf3618903ec8586dfc27f8d6ff33a591d65aa
Martin Pfeiffer <hubipete@gmx.net>
2005-06-28 17:42:52 +0000

    Removed some stupid typo
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=429732

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
                      KAboutData::License_GPL, "(C) 2002-2005 Carsten Niehaus", 0, "http://edu.kde.org/kalzium");
     about.addAuthor( "Carsten Niehaus", 0, "cniehaus@kde.org" );
     about.addCredit( "Pino Toscano", I18N_NOOP("Code contributions" ));
-    about.addCredit( "Martin Pfeiffer", I18N_NOOP("Tolltip, some other small things" ));
+    about.addCredit( "Martin Pfeiffer", I18N_NOOP("Tooltip, some other small things" ));
     about.addCredit( "Marco Martin", I18N_NOOP("Thank you for some icons and inspiration for others" ));
     about.addCredit( "Chris Luetchford", I18N_NOOP("svg icon"), "chris@os11.com");
     about.addCredit( "Anne-Marie Mahfouf", I18N_NOOP( "A lot of small things and the documentation" ));
