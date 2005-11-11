3989836cd672da0d7bad6a1bfd35aadf8a202063
Carsten Niehaus <cniehaus@gmx.de>
2005-11-11 14:15:49 +0000

    propertysliderwidget work
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=479714

--- src/propertysliderwidget.cpp
+++ src/propertysliderwidget.cpp
@@ -1,17 +1,49 @@
 #include "propertysliderwidget.h"
+#include <QSlider>
 
 PropertySliderWidget::PropertySliderWidget( QWidget * parent )
 	: QWidget( parent )
 {
+	setupUi( this );
 }
 
 PropertySliderWidget::~PropertySliderWidget()
 {
 }
 
-void PropertySliderWidget::setValues( QList<QVariant*> list )
+void PropertySliderWidget::setValues( QList<int> list )
 {
 	m_list = list;
+}
+
+void PropertySliderWidget::valueChanged( int value )
+{
+	m_value = value;
+}
+
+void PropertySliderWidget::slotValueChanged( int value )
+{
+	emit valueHasChanged( value );
+}
+
+/*********************************************************/
+
+void TimeSliderWidget::slotValueChanged( int value )
+{
+	emit valueHasChanged( value );
+}
+
+TimeSliderWidget::TimeSliderWidget( QWidget * parent )
+	: PropertySliderWidget( parent )
+{
+}
+
+TimeSliderWidget::~TimeSliderWidget()
+{}
+
+int TimeSliderWidget::value() const
+{
+	return time_slider->value();
 }
 
 #include "propertysliderwidget.moc"
--- src/propertysliderwidget.h
+++ src/propertysliderwidget.h
@@ -5,6 +5,8 @@
 #include <QList>
 #include <QVariant>
 
+#include "timewidget.h"
+
 /**
  * @author Carsten Niehaus <cniehaus@kde.org>
  *
@@ -13,7 +15,7 @@
  * example, the dateslider which shows which elements where
  * known at a certain date, is based on this class.
  */
-class PropertySliderWidget : public QWidget
+class PropertySliderWidget : public QWidget, protected Ui_TimeWidget
 {
 	Q_OBJECT
 
@@ -27,28 +29,64 @@ class PropertySliderWidget : public QWidget
 		/**
 		 * Destructor
 		 */
-		~PropertySliderWidget();
+		virtual ~PropertySliderWidget();
 
 		/**
 		 * Set the list @p list as the internal valuelist
 		 * @param list the list of the values of the property
 		 */
-		void setValues( QList<QVariant*> list );
+		void setValues( QList<int> list );
 
 		/**
 		 * @return the currently selected value
 		 */
-		virtual QVariant value() const = 0;
+		virtual int value() const = 0;
+
+	private:
+		int m_value;
+
+	protected slots:
+		/**
+		 * this slot reacts to a new value
+		 */
+		virtual void slotValueChanged( int value ) = 0;
+
+	protected:
+		QList<int> m_list;
+
+	protected slots:
+		/**
+		 * emitted when the value changes
+		 * @param the new value
+		 */
+		virtual void valueChanged( int value );
+
+	signals:
+		void valueHasChanged( int value );
+};
+
+class TimeSliderWidget : public PropertySliderWidget
+{
+	Q_OBJECT
+
+	public:
+		TimeSliderWidget( QWidget * parent );
+		
+		/**
+		 * Destructor
+		 */
+		virtual ~TimeSliderWidget();
+		
+		/**
+		 * @return the currently selected value
+		 */
+		virtual int value() const;
 
 	private slots:
 		/**
 		 * this slot reacts to a new value
 		 */
-		virtual void slotValueChanged( QVariant value ) = 0;
-
-	private:
-		QList<QVariant*> m_list;
-
+		virtual void slotValueChanged( int value );
 };
 #endif // PROPERTYSLIDERWIDGET_H
 
