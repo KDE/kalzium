270a8551de9860db8aa6f0e56a6ad52b425ba1cf
Carsten Niehaus <cniehaus@gmx.de>
2006-01-30 12:12:54 +0000

    * Moving code around to reduce filesize
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=503787

--- games/games.h
+++ games/games.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) 2005      by Carsten Niehaus,    cniehaus@kde.org       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.          *
+ ***************************************************************************/
 #ifndef GAMES_H
 #define GAMES_H
 
@@ -5,14 +23,9 @@
 
 #include <krandomsequence.h>
 #include <kmainwindow.h>
+#include <kdialog.h>
 
 class KRandomSequence;
-
-class GamesDialog : public KMainWindow
-{
-	public:
-		GamesDialog();
-};
 
 /**
  * @author Carsten Niehaus
---
+++ games/main.cpp
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *   Copyright (C) 2005      by Carsten Niehaus,    cniehaus@kde.org       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.          *
+ ***************************************************************************/
+#include <kapplication.h>
+
+#include <QPoint>
+#include <QLayout>
+
+#include <kdebug.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <klocale.h>
+
+#include "gamesdialog.h"
+
+static KCmdLineOptions options[] =
+{
+    KCmdLineLastOption
+};
+
+int main(int argc, char **argv)
+{
+    KAboutData about("games", I18N_NOOP("Kalzium-Games"), "0.1", "Statistical Games",
+                     KAboutData::License_GPL, "(C) 2002-2005 Carsten Niehaus", 0, "http://edu.kde.org/kalzium");
+    about.addAuthor( "Carsten Niehaus", 0, "cniehaus@kde.org" );
+    
+	KCmdLineArgs::init(argc, argv, &about);
+    KCmdLineArgs::addCmdLineOptions( options );
+    
+	KApplication app;
+    GamesDialog *mainWin = 0;
+
+    if (app.isSessionRestored())
+    {
+    }
+    else
+    {
+        // no session.. just start up normally
+        KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+        mainWin = new GamesDialog();
+        app.setMainWidget( mainWin );
+        mainWin->show();
+
+        args->clear();
+    }
+
+    // mainWin has WDestructiveClose flag by default, so it will delete itself.
+    return app.exec();
+}
+
