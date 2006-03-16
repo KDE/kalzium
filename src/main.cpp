8860f1fdec9de836c2d89b425af148847a11b4c5
Carsten Niehaus <cniehaus@gmx.de>
2006-03-16 21:48:51 +0000

    increase version number for 3.5.2
    svn path=/branches/KDE/3.5/kdeedu/kalzium/; revision=519337

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 #include "config.h"
 #include "kalzium.h"
 
-#define APP_VERSION "1.4.3"
+#define APP_VERSION "1.4.4"
 
 static const char description[] =
     I18N_NOOP("A periodic table of the elements");
