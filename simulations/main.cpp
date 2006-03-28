d7e0aa38a12c5fc34d36d0e504c3014ba88634ba
Carsten Niehaus <cniehaus@gmx.de>
2006-03-28 10:26:40 +0000

    next try to make all this compile... Kalzium is compiling, only this app gives me a headache... the .ui-files are not used (converted by uic...)
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=523478

--- simulations/main.cpp
+++ simulations/main.cpp
@@ -17,18 +17,69 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.          *
  ***************************************************************************/
 
-#include <QtGui/QApplication>
-
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <klocale.h>
 #include "maindialog_impl.h"
 
-int main( int argc, char *argv[] )
+#include "config.h"
+#include "kalzium.h"
+
+#define APP_VERSION "0.1"
+
+static const char description[] =
+    I18N_NOOP("Simulations" );
+
+static const char version[] = APP_VERSION;
+
+static KCmdLineOptions options[] =
 {
-	QApplication app( argc, argv );
+//    { "+[URL]", I18N_NOOP( "Document to open" ), 0 },
+    KCmdLineLastOption
+};
 
-	MainDialog_Impl* window = new MainDialog_Impl( );
+#ifdef HAVE_FACILE
+//X extern "C" {
+//X         void caml_startup(char**);
+//X };
+#endif
 
-	window->show();
+int main(int argc, char **argv)
+{
+#ifdef HAVE_FACILE
+//X     caml_startup(argv);
+#endif
 
-	return app.exec();
+    KAboutData about("kalzium", I18N_NOOP("Simulations"), version, description,
+                     KAboutData::License_GPL, "(C) 2005-2006 Carsten Niehaus", 0, "http://edu.kde.org/kalzium");
+    about.addAuthor( "Carsten Niehaus", 0, "cniehaus@kde.org" );
+    about.addCredit( "Pino Toscano", I18N_NOOP("Code contributions" ));
+    KCmdLineArgs::init(argc, argv, &about);
+    KCmdLineArgs::addCmdLineOptions( options );
+    KApplication app;
+    MainDialog_Impl *mainWin = 0;
+
+    if (app.isSessionRestored())
+    {
+        RESTORE(MainDialog_Impl);
+    }
+    else
+    {
+        // no session.. just start up normally
+        KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+        /// @todo do something with the command line args here
+
+        mainWin = new MainDialog_Impl();
+        app.setMainWidget( mainWin );
+        mainWin->show();
+
+        args->clear();
+    }
+
+    // mainWin has WDestructiveClose flag by default, so it will delete itself.
+    return app.exec();
 }
+
 
