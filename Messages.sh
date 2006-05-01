05e1202be444a5da03216c8de2ceecf5146b7b7c
Stephan Kulow <coolo@kde.org>
2006-05-01 19:08:32 +0000

    moved messages targets out of Makefile.am
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=536286

---
+++ Messages.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+$EXTRACTRC src/*.ui src/*.rc src/*.kcfg >> rc.cpp || exit 11
+$EXTRACTATTR --attr=name,origin data/data.xml >> rc.cpp || exit 12
+$EXTRACTRC --tag-group=none --tag=name data/data.xml >> rc.cpp || exit 13
+$EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
+$EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
+$XGETTEXT src/*.cpp -o $podir/kalzium.pot
