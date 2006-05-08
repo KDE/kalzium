d9dbd346e48a5fa80a46867f9e4ac58a28257e35
Laurent Montel <montel@kde.org>
2006-05-08 11:11:11 +0000

    Adapt to bnew kdelibs api
    svn path=/trunk/KDE/kdeedu/libkdeedu/kdeeduui/kdeeduglossary.cpp; revision=538576

--- kdeeduui/kdeeduglossary.cpp
+++ kdeeduui/kdeeduglossary.cpp
@@ -239,7 +239,7 @@ GlossaryDialog::GlossaryDialog( bool folded, QWidget *parent )
  
 	m_search->setListView( m_glosstree );
  
-	m_htmlpart = new KHTMLPart( vs, "html-part" );
+	m_htmlpart = new KHTMLPart( vs );
 
 	connect( m_htmlpart->browserExtension(), SIGNAL( openURLRequestDelayed( const KUrl &, const KParts::URLArgs & ) ), this, SLOT( displayItem( const KUrl &, const KParts::URLArgs & ) ) );
 	connect( m_glosstree, SIGNAL(clicked( Q3ListViewItem * )), this, SLOT(slotClicked( Q3ListViewItem * )));
