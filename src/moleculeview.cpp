dff020897168b6930ce2fbde3a3b856e74011342
Carsten Niehaus <cniehaus@gmx.de>
2006-06-12 13:23:30 +0000

    Making reading of chemical datafiles work. I created a small OB-wrapper to hide the ugly OB-API
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=550626

--- src/moleculeview.cpp
+++ src/moleculeview.cpp
@@ -13,41 +13,18 @@
  ***************************************************************************/
 #include <kdebug.h>
 #include "moleculeview.h"
+#include "openbabel2wrapper.h"
 
+#include <QString>
 #include <QMouseEvent>
 #include <QLayout>
 #include <QListWidget>
-
-//includes for OpenBabel2
-#include <openbabel/mol.h>
-#include <openbabel/obiter.h>
-
-using namespace OpenBabel;
+#include <QMessageBox>
 
 MoleculeWidget::MoleculeWidget( QWidget * parent )
 	: KalziumGLWidget( parent )
 {
 	setMinimumSize( 100,100 );
-//this is how to use OpenBabel
-	OBAtom *atom;
-	FOR_NBORS_OF_ATOM( nbr, atom )
-	{
-		double x,y,z;
-		x = atom->GetX();
-		y = atom->GetY();
-		z = atom->GetZ();
-	}
-
-//X 	FOR_BONDS_OF_ATOM( bonds, bond )
-//X 	{
-//X 		atom = bond->GetBeginAtom();
-//X 		if ( bond->isTripple() )//draw three lines
-//X 		{}
-//X 		if ( bond->isDouble() )//draw two lines
-//X 		{}
-//X 		if ( bond->isSingle() )//one line
-//X 		{}
-//X 	}
 }
 
 MoleculeWidget::~MoleculeWidget()
@@ -89,6 +66,7 @@ MoleculeDialog::MoleculeDialog( QWidget * parent )
 	i18n( "Molecular Viewer" ),
 	Help|Close )
 {
+	
 	QWidget * dummy = new QWidget();
 	setMainWidget( dummy );
 
@@ -101,6 +79,8 @@ MoleculeDialog::MoleculeDialog( QWidget * parent )
 	hbox->addWidget( m_moleculeWidget );
 	vbox->addWidget( m_listView );
 	dummy->setLayout( hbox );
+
+	OpenBabel2Wrapper::readMolecule( "/home/carsten/test.cml" );
 
 	fillList();
 }
---
+++ src/openbabel2wrapper.cpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+    copyright            : (C) 2006 by Carsten Niehaus
+    email                : cniehaus@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "openbabel2wrapper.h"
+
+#include <kdebug.h>
+#include <klocale.h>
+#include <QMessageBox>
+
+#include <sstream>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <malloc.h>
+
+
+#include <QString>
+#include <QMouseEvent>
+#include <QLayout>
+#include <QListWidget>
+#include <QMessageBox>
+
+
+OpenBabel::OBMol* OpenBabel2Wrapper::readMolecule( const QString& filename )
+{
+	OpenBabel::OBConversion Conv;
+	OpenBabel::OBFormat *inFormat = NULL;
+
+	//the OB2 Molecule
+	OpenBabel::OBMol *mol = new OpenBabel::OBMol;
+	ifstream inFileStream(  filename.toLatin1() );
+	if ( !inFileStream ) {
+		QMessageBox::warning(  0, i18n( "Problem while opening the file" ),
+				i18n( "Cannot open the specified file." ) );
+		return 0;
+	}
+
+	//find out which format the file has...
+	inFormat = Conv.FormatFromExt( filename.toLatin1() );
+	Conv.SetInAndOutFormats( inFormat,inFormat );
+	Conv.Read( mol, &inFileStream );
+
+	kDebug() << QString::fromLatin1( mol->GetFormula().c_str() )  << " (Weight: " << mol->GetMolWt() << ", Title: "<< mol->GetTitle() << ")" << endl;
+
+	return mol;
+}
