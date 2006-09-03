312a157a2933fc1f07066923bd3257d270059876
Carsten Niehaus <cniehaus@gmx.de>
2006-09-03 08:09:20 +0000

    It seems Qt 4.2tp1 removed some includes...
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=580296

--- src/kalziumglhelperclasses.h
+++ src/kalziumglhelperclasses.h
@@ -14,6 +14,7 @@
  ***************************************************************************/
 #include <math.h>
 #include <QGLWidget>
+#include <GL/glu.h>
 #include <QPainter>
 #include <QImage>
 #include <QChar>
