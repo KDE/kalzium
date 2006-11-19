68aa98c224cab133bb7d64bf77b52a21e7786797
Carsten Niehaus <cniehaus@gmx.de>
2006-11-19 09:56:50 +0000

    * Fix Copyright header stuff
    svn path=/trunk/KDE/kdeedu/libkdeedu/libscience/; revision=606098

--- libscience/isotope.cpp
+++ libscience/isotope.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Carsten Niehaus                                 *
- *   cniehaus@kde.org                                                      *
+ *   Copyright (C) 2005 by Carsten Niehaus <cniehaus@kde.org>              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
