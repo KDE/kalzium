408046282fe4634c26261211a68141857484ba04
Pino Toscano <pino@kde.org>
2007-01-03 11:18:15 +0000

    isolate the configure checks and the related cmake stuff of kalzium in a separate file, and make kalzium compile again even without eigen
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=619419

---
+++ KalziumConfigureChecks.cmake
@@ -0,0 +1,24 @@
+include(CheckIncludeFiles)
+include(CheckFunctionExists)
+
+macro_optional_find_package(OCaml)
+macro_optional_find_package(Libfacile)
+macro_optional_find_package(OpenBabel2)
+macro_optional_find_package(OpenGL)
+macro_optional_find_package(Eigen)
+
+macro_bool_to_01(LIBFACILE_FOUND HAVE_FACILE)
+macro_bool_to_01(OPENBABEL2_FOUND HAVE_OPENBABEL2)
+macro_bool_to_01(OPENGL_FOUND HAVE_OPENGL)
+
+# at the end, output the configuration
+configure_file(
+   ${CMAKE_CURRENT_SOURCE_DIR}/config-kalzium.h.cmake
+   ${CMAKE_CURRENT_BINARY_DIR}/config-kalzium.h
+)
+
+macro_log_feature(OCAML_FOUND "OCaml" "OCaml is needed by Kalzium" "http://caml.inria.fr/" FALSE "" "")
+macro_log_feature(LIBFACILE_FOUND "libfacile" "libfacile is needed by Kalzium" "http://www.recherche.enac.fr/log/facile/" FALSE "" "")
+macro_log_feature(OPENGL_FOUND "libopengl" "OpenGl lib is needed by Kalzium" "http://www.opengl.org" FALSE "" "")
+macro_log_feature(OPENBABEL2_FOUND "libopenbabel" "OpenBabel is needed by Kalzium" "http://openbabel.sourceforge.net/" FALSE "2.1" "")
+
