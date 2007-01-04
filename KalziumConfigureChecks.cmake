3ac0a96f902625ea90855a05eb2b4812694e7400
Pino Toscano <pino@kde.org>
2007-01-04 11:38:48 +0000

    OpenBabel2, OpenGL and Eigen checks are needed for componentviewer of libkdeeduscience
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=619771

--- KalziumConfigureChecks.cmake
+++ KalziumConfigureChecks.cmake
@@ -3,9 +3,6 @@ include(CheckFunctionExists)
 
 macro_optional_find_package(OCaml)
 macro_optional_find_package(Libfacile)
-macro_optional_find_package(OpenBabel2)
-macro_optional_find_package(OpenGL)
-macro_optional_find_package(Eigen)
 
 macro_bool_to_01(LIBFACILE_FOUND HAVE_FACILE)
 macro_bool_to_01(OPENBABEL2_FOUND HAVE_OPENBABEL2)
@@ -19,7 +16,4 @@ configure_file(
 
 macro_log_feature(OCAML_FOUND "OCaml" "OCaml is needed by Kalzium" "http://caml.inria.fr/" FALSE "" "")
 macro_log_feature(LIBFACILE_FOUND "LibFacile" "libfacile is needed by Kalzium" "http://www.recherche.enac.fr/log/facile/" FALSE "" "")
-macro_log_feature(OPENGL_FOUND "OpenGL" "OpenGL is needed by Kalzium" "http://www.opengl.org" FALSE "" "")
-macro_log_feature(OPENBABEL2_FOUND "OpenBabel2" "OpenBabel is needed by Kalzium" "http://openbabel.sourceforge.net/" FALSE "2.1" "")
-macro_log_feature(EIGEN_FOUND "Eigen" "Eigen is needed by Kalzium" "http://eigen.tuxfamily.org" FALSE "1.0.1")
 
