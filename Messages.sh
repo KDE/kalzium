eff6f8854aa0e6043bc214fd80b5513b815f45ab
Stephan Kulow <coolo@kde.org>
2007-03-06 10:49:01 +0000

    oops, data.xml was here twice
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=639949

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC src/*.ui src/*.rc src/*.kcfg >> rc.cpp || exit 11
-$EXTRACTATTR --attr=name,origin data/data.xml >> rc.cpp || exit 12
+#$EXTRACTATTR --attr=name,origin data/data.xml >> rc.cpp || exit 12
 #$EXTRACTRC --tag-group=none --tag=name data/data.xml >> rc.cpp || exit 13
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
