e20902ab7d67157b9468e29f980ff60380bb55d8
Benoît Jacob <jacob.benoit.1@gmail.com>
2007-05-23 08:28:53 +0000

    perform a basic form of frustum culling. When navigating through a large molecule, this increases the framerate by somewhere between +10% and +50%. Merging to libavogadro upstream.
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=667620

--- libavogadro-kalzium/src/painter.cpp
+++ libavogadro-kalzium/src/painter.cpp
@@ -228,7 +228,14 @@ namespace Avogadro
   void Painter::drawSphere( const Eigen::Vector3d & center, double radius ) const
   {
     assert( d->initialized );
-    double apparentRadius = radius / d->widget->camera()->distance(center);
+    Eigen::Vector3d transformedCenter = d->widget->camera()->modelview() * center;
+    double distance = transformedCenter.norm();
+    
+    // perform a rough form of frustum culling
+    double dot = transformedCenter.z() / distance;
+    if(dot > -0.6) return;
+    
+    double apparentRadius = radius / distance;
     int detailLevel = 1 + static_cast<int>( floor(
           PAINTER_SPHERES_DETAIL_COEFF * (sqrt(apparentRadius) - PAINTER_SPHERES_SQRT_LIMIT_MIN_LEVEL)
           ) );
@@ -253,7 +260,15 @@ namespace Avogadro
       double radius) const
   {
     assert( d->initialized );
-    double apparentRadius = radius / d->widget->camera()->distance(end1);
+    
+    Eigen::Vector3d transformedEnd1 = d->widget->camera()->modelview() * end1;
+    double distance = transformedEnd1.norm();
+    
+    // perform a rough form of frustum culling
+    double dot = transformedEnd1.z() / distance;
+    if(dot > -0.6) return;
+    
+    double apparentRadius = radius / distance;
     int detailLevel = 1 + static_cast<int>( floor(
           PAINTER_CYLINDERS_DETAIL_COEFF
           * (sqrt(apparentRadius) - PAINTER_CYLINDERS_SQRT_LIMIT_MIN_LEVEL)
