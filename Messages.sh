dce93b16731816a0da98b8ba53d1e64b1d92a6ea
Albert Astals Cid <tsdgeos@terra.es>
2007-09-05 22:17:00 +0000

    rc.cpp is not in src/ so it was not beign extracted
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=708881

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 $EXTRACTRC src/*.ui compoundviewer/*.cpp libavogadro-kalzium/src/*.cpp libavogadro-kalzium/src/engines/*.cpp libavogadro-kalzium/src/tools/*.cpp src/*.rc src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
-$XGETTEXT src/*.cpp -o $podir/kalzium.pot
+$XGETTEXT rc.cpp src/*.cpp -o $podir/kalzium.pot
