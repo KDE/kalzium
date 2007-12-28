c9b4cdbcb28efe963f550f3239f1cfa352a6b0f1
Pino Toscano <pino@kde.org>
2007-12-28 17:57:08 +0000

    Fix message extraction.
    CCMAIL: kde-i18n-doc@kde.org
    
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=753887

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC src/*.ui compoundviewer/*.cpp libavogadro-kalzium/src/*.cpp libavogadro-kalzium/src/engines/*.cpp libavogadro-kalzium/src/tools/*.cpp src/*.rc src/*.kcfg >> rc.cpp || exit 11
+$EXTRACTRC `find src -name \*.ui` src/*.rc src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
-$XGETTEXT rc.cpp src/*.cpp -o $podir/kalzium.pot
+$XGETTEXT rc.cpp compoundviewer/*.cpp libavogadro-kalzium/src/*.cpp libavogadro-kalzium/src/engines/*.cpp libavogadro-kalzium/src/tools/*.cpp `find src -name \*.cpp` -o $podir/kalzium.pot
