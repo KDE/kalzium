38b828e153a7d357530a18153c992f13cb195acc
Laurent Montel <montel@kde.org>
2008-06-24 06:57:18 +0000

    libavogadron use tr macro => extract all strings now
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=823792

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,5 @@
 $EXTRACTRC `find src -name \*.ui` src/*.rc src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
-$XGETTEXT rc.cpp compoundviewer/*.cpp libavogadro-kalzium/src/*.cpp libavogadro-kalzium/src/engines/*.cpp libavogadro-kalzium/src/tools/*.cpp `find src -name \*.cpp` -o $podir/kalzium.pot
+$XGETTEXT -ktr:1,1t -ktr:1,2c,2t -kQT_TRANSLATE_NOOP:1c,2,2t -kQT_TR_NOOP:1,1t -ktranslate:1c,2,2t -ktranslate:2,3c,3t rc.cpp compoundviewer/*.cpp libavogadro-kalzium/src/*.cpp libavogadro-kalzium/src/engines/*.cpp libavogadro-kalzium/src/tools/*.cpp `find src -name \*.cpp` -o $podir/kalzium.pot
+rm -f rc.cpp
