c15c55d98c40e24a2359d1f059cbad120133d679
Pino Toscano <pino@kde.org>
2009-05-07 14:01:33 +0000

    the current avogradro snapshot does not have eigen1, but requires an external eigen2 thus, do not force eigen as "found", but tie the HAVE_EIGEN definition to the result of the eigen2 check
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=964861

--- KalziumConfigureChecks.cmake
+++ KalziumConfigureChecks.cmake
@@ -6,19 +6,10 @@ macro_optional_find_package(Libfacile)
 macro_optional_find_package(OpenBabel2)
 macro_optional_find_package(OpenGL)
 
-# Since Kalzium's old snapshot of libavogadro is the last remaining piece of code using eigen1,
-# we remove eigen1 from kdesupport and instead add a copy of it in our snapshot of libavogadro.
-# Yes, this is very ugly. But eigen1 is much smaller than libavogadro anyway. So this doesn't make
-# things much worse. The real big problem is that we use a snapshot of libavogadro in the first place.
-# I really want this to go away in 4.3, normally by relying directly on libavogadro (which should at last
-# make a stable release), or else we just move the 3D molecular editor to playground/.
-# macro_optional_find_package(Eigen)
-set(EIGEN_FOUND TRUE)
-
 macro_bool_to_01(LIBFACILE_FOUND HAVE_FACILE)
 macro_bool_to_01(OPENBABEL2_FOUND HAVE_OPENBABEL2)
 macro_bool_to_01(OPENGL_FOUND HAVE_OPENGL)
-macro_bool_to_01(EIGEN_FOUND HAVE_EIGEN)
+macro_bool_to_01(EIGEN2_FOUND HAVE_EIGEN)
 
 # at the end, output the configuration
 configure_file(
