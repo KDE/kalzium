853ab8faf5e7f692a50448facf4c9c9dc039c941
Kashyap Ramesh Puranik <kashthealien@gmail.com>
2009-06-04 05:35:20 +0000

    Adding copyright information to remove some issues from krazy (english breakfast network)
    svn path=/branches/kalzium/gsoc2009/kalzium/; revision=977409

--- src/plasmoids/gasPlasmoid/gasCalculator.h
+++ src/plasmoids/gasPlasmoid/gasCalculator.h
@@ -1,3 +1,15 @@
+/***************************************************************************
+    copyright            : (C) 2009 by Kashyap R Puranik
+    email                : kashthealien@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 // Here we avoid loading the header multiple times
 #ifndef gasCalculator_HEADER
 #define gasCalculator_HEADER
