d5085d7a6e9dc37de45fa1d0b2333ad5c26dd9c1
Pino Toscano <pino@kde.org>
2009-07-19 08:50:00 +0000

    don't extract messages from the libconversion copy into kalzium.pot, they go already in an own .pot
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=999088

--- Messages.sh
+++ Messages.sh
@@ -3,5 +3,5 @@ $EXTRACTRC `find libavogadro-kalzium src -name \*.ui` src/*.rc src/*.kcfg >> rc.
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
 $XGETTEXT_QT `find libavogadro-kalzium -name \*.cpp` -o $podir/kalzium_qt.pot
-$XGETTEXT rc.cpp compoundviewer/*.cpp `find src -name \*.cpp` -o $podir/kalzium.pot
+$XGETTEXT rc.cpp compoundviewer/*.cpp `find src -name \*.cpp | grep -v 'src/conversion'` -o $podir/kalzium.pot
 
