6991324b85fabe96d39a80a86965103593a8ee9c
Burkhard Lück <lueck@hube-lueck.de>
2009-09-25 23:00:18 +0000

    extract element names and use them in the engine, makes 238 new messages for 4.3 CCMAIL:kde-i18n-doc@kde.org
    svn path=/branches/KDE/4.3/kdeedu/kalzium/; revision=1028128

--- plasmoid/engine/Messages.sh
+++ plasmoid/engine/Messages.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
+$EXTRACTATTR --context=Elementname --attr=label,value ../../../libkdeedu/libscience/data/elements.xml >>rc.cpp
 $XGETTEXT *.cpp -o $podir/plasma_engine_kalzium.pot
