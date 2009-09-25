e6e39e06285dc4b2288c7e4a35d23d7809cedea7
Burkhard Lück <lueck@hube-lueck.de>
2009-09-25 23:12:38 +0000

    revert message extraction, these strings are already in libkdedu. thanks to pino for the hint
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=1028137

--- plasmoid/engine/Messages.sh
+++ plasmoid/engine/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTATTR --context=Elementname --attr=label,value ../../../libkdeedu/libscience/data/elements.xml >>rc.cpp
 $XGETTEXT *.cpp -o $podir/plasma_engine_kalzium.pot
