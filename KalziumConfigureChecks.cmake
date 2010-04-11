c8e20f7fcc94faaa839798e6dc0fc7237e02c3de
Pino Toscano <pino@kde.org>
2010-04-11 11:04:56 +0000

    no more need to check and use opengl directly
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=1113636

--- KalziumConfigureChecks.cmake
+++ KalziumConfigureChecks.cmake
@@ -4,13 +4,11 @@ include(CheckFunctionExists)
 macro_optional_find_package(OCaml)
 macro_optional_find_package(Libfacile)
 macro_optional_find_package(OpenBabel2)
-macro_optional_find_package(OpenGL)
 macro_optional_find_package(Eigen2)
 macro_optional_find_package(Avogadro 1.0.0)
 
 macro_bool_to_01(LIBFACILE_FOUND HAVE_FACILE)
 macro_bool_to_01(OPENBABEL2_FOUND HAVE_OPENBABEL2)
-macro_bool_to_01(OPENGL_FOUND HAVE_OPENGL)
 macro_bool_to_01(EIGEN2_FOUND HAVE_EIGEN)
 macro_bool_to_01(Avogadro_FOUND HAVE_AVOGADRO)
 
@@ -22,7 +20,6 @@ configure_file(
 
 macro_log_feature(OCAML_FOUND "OCaml" "OCaml is needed by Kalzium for the Equation Solver" "http://caml.inria.fr/" FALSE "" "")
 macro_log_feature(LIBFACILE_FOUND "LibFacile" "libfacile is needed by Kalzium for the Equation Solver." "http://www.recherche.enac.fr/log/facile/" FALSE "" "")
-macro_log_feature(OPENGL_FOUND "OpenGL" "OpenGL is needed by Kalzium for the 3D-molecule viewer" "http://www.opengl.org" FALSE "" "")
 macro_log_feature(OPENBABEL2_FOUND "OpenBabel2" "OpenBabel is needed by Kalzium for the 3D-molecule viewer" "http://openbabel.sourceforge.net/" FALSE "2.2" "")
 macro_log_feature(EIGEN2_FOUND "Eigen2" "A generic C++ template library for dense and sparse matrices" "http://eigen.tuxfamily.org" FALSE "2.0.0" "")
 macro_log_feature(Avogadro_FOUND "Avogadro" "Advanced molecular editor" "http://avogadro.openmolecules.net" FALSE "1.0" "")
