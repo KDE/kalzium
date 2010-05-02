a23184564ac886f675ef6e59ff7c76e9a7c220ec
Pino Toscano <pino@kde.org>
2010-05-02 22:55:19 +0000

    no more avogadro-kalzium, so no more kalzium_qt, yay!
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=1122008

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
-$EXTRACTRC `find libavogadro-kalzium src -name \*.ui` src/*.rc src/*.kcfg >> rc.cpp || exit 11
+$EXTRACTRC `find src -name \*.ui` src/*.rc src/*.kcfg >> rc.cpp || exit 11
 $EXTRACTRC --tag-group=none --tag=name --tag=desc --tag=refitem data/knowledge.xml >> rc.cpp || exit 14
 $EXTRACTRC --tag-group=none --tag=name --tag=desc data/tools.xml >> rc.cpp || exit 15
-$XGETTEXT_QT `find libavogadro-kalzium -name \*.cpp` -o $podir/kalzium_qt.pot
 $XGETTEXT rc.cpp compoundviewer/*.cpp `find src -name \*.cpp | grep -v 'src/conversion'` -o $podir/kalzium.pot
 
