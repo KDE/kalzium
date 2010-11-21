0903fbac61d4fa8bb88a54e0bf0f29bbc0669cc4
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2010-11-21 20:48:04 +0000

    formatting the legendwidget. SVN_SILENT
    svn path=/trunk/KDE/kdeedu/kalzium/; revision=1199427

--- src/legendwidget.cpp
+++ src/legendwidget.cpp
@@ -32,7 +32,6 @@ LegendWidget::LegendWidget( QWidget *parent )
 {
     m_update = true;
     m_dockArea = Qt::BottomDockWidgetArea;
-//     setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
 }
 
 void LegendWidget::setElementProperty(KalziumElementProperty* propety)
@@ -64,32 +63,32 @@ void LegendWidget::updateContent()
     if (m_update) {
         QString gradientDesc;
         QList< QPair<QString, QColor> > items;
-	// Handel different Gradients
+        // Handle different Gradients
         switch ( m_elementProerty->gradientId() ) {
-	  case KalziumElementProperty::NOGRADIENT: // None
-           break;
+        case KalziumElementProperty::NOGRADIENT: // None
+            break;
 
-	  case KalziumElementProperty::SOMGradientType:
-	    items << qMakePair( m_elementProerty->gradient()->description(), QColor());
+        case KalziumElementProperty::SOMGradientType:
+            items << qMakePair( m_elementProerty->gradient()->description(), QColor());
             items << qMakePair( i18nc("one of the three states of matter (solid, liquid, vaporous or unknown)", "Solid" ), QColor( Prefs::color_solid() ));
             items << qMakePair( i18nc("one of the three states of matter (solid, liquid, vaporous or unknown)", "Liquid" ), QColor( Prefs::color_liquid() ) );
             items << qMakePair( i18nc("one of the three states of matter (solid, liquid, vaporous or unknown)", "Vaporous" ), QColor( Prefs::color_vapor() ) );
             items << qMakePair( i18nc("one of the three states of matter (solid, liquid, vaporous or unknown)", "Unknown" ), QColor( Qt::lightGray ) );
             break;
 
-	  default:
+        default:
             if (m_elementProerty->gradient()->logarithmicGradient())
                 gradientDesc = i18nc("one of the two types of gradients available", "logarithmic");
             else
                 gradientDesc = i18nc("one of the two types of gradients available", "linear");
             items << qMakePair( i18n( "%1 (%2)" ,m_elementProerty->gradient()->description(), gradientDesc ), QColor() );
-            items << qMakePair( i18nc( "Minimum value of the gradient" , "Minimum: %1" , QString::number(m_elementProerty->gradient()->minValue()) + m_elementProerty->gradient()->unit()), QColor( m_elementProerty->gradient()->firstColor() ));
-            items << qMakePair( i18nc( "Maximum value of the gradient" , "Maximum: %1" , QString::number(m_elementProerty->gradient()->maxValue()) + m_elementProerty->gradient()->unit()), QColor( m_elementProerty->gradient()->secondColor() ));
+            items << qMakePair( i18nc( "Minimum value of the gradient" , "Minimum: %1" , QString::number(m_elementProerty->gradient()->minValue()) + " " + m_elementProerty->gradient()->unit()), QColor( m_elementProerty->gradient()->firstColor() ));
+            items << qMakePair( i18nc( "Maximum value of the gradient" , "Maximum: %1" , QString::number(m_elementProerty->gradient()->maxValue()) + " " + m_elementProerty->gradient()->unit()), QColor( m_elementProerty->gradient()->secondColor() ));
             break;
         }
         // schemes are always there
-	items << qMakePair( i18n( "Scheme: %1" ,m_elementProerty->scheme()->description() ), QColor() );
-	items << m_elementProerty->scheme()->legendItems();
+        items << qMakePair( i18n( "Scheme: %1" ,m_elementProerty->scheme()->description() ), QColor() );
+        items << m_elementProerty->scheme()->legendItems();
 
         updateLegendItemLayout( items );
     }
@@ -97,7 +96,7 @@ void LegendWidget::updateContent()
 
 void LegendWidget::updateLegendItemLayout( const QList<legendPair>& list )
 {
-    if (layout()) {
+    if ( layout() ) {
         delete layout();
     }
     foreach ( LegendItem * i, m_legendItemList ) {
@@ -118,9 +117,8 @@ void LegendWidget::updateLegendItemLayout( const QList<legendPair>& list )
         LegendItem *item = new LegendItem( pair );
         m_legendItemList.append(item);
 
-	// FIXME With only dockwidget's there is no bottom and Top.
         if ((m_dockArea == Qt::BottomDockWidgetArea || m_dockArea == Qt::TopDockWidgetArea) ) {
-            if (!pair.second.isValid() ) {
+            if ( !pair.second.isValid() ) {
                 y++;
                 x = 0;
             }
@@ -129,7 +127,7 @@ void LegendWidget::updateLegendItemLayout( const QList<legendPair>& list )
                 x = 1;
             }
         }
-        layout->addWidget(item , x, y);
+        layout->addWidget( item , x, y );
         x++;
     }
     setLayout( layout );
@@ -140,6 +138,7 @@ LegendItem::LegendItem(const QPair<QString, QColor>& pair, QWidget * parent)
 {
     Q_UNUSED(parent);
     QHBoxLayout *ItemLayout = new QHBoxLayout(this);
+    ItemLayout->setMargin(1);
     QLabel * LegendLabel = new QLabel(this);
 
     if ( pair.second.isValid() ) {
