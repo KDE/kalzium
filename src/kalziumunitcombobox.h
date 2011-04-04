b618e294f0bdbf8119967f6331f736186ea786ce
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2011-04-04 22:21:56 +0200

    unit comboboxes for kalzium.

---
+++ src/kalziumunitcombobox.h
@@ -0,0 +1,35 @@
+/*
+    Copyright (C) 2011  Rebetez Etienne <etienne.rebetez@oberwallis.ch>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef KALZIUMUNITCOMBOBOX_H
+#define KALZIUMUNITCOMBOBOX_H
+
+#include <QComboBox>
+
+class KalziumUnitCombobox : public QComboBox
+{
+    Q_OBJECT
+
+public:
+    KalziumUnitCombobox( const QList< int > &unitList, QWidget *parent = 0 );
+
+    int getCurrentUnitId() const;
+
+};
+
+#endif // KALZIUMUNITCOMBOBOX_H
