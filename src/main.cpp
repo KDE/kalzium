529d5f9ae372f29901addbcb5ac09a7b8aeba754
Etienne Rebetez <etienne.rebetez@gmail.com>
2014-03-16 17:48:43 +0100

    Update version for kde 4.14

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 
 #include <config-kalzium.h>
 
-#define APP_VERSION "2.4.01"
+#define APP_VERSION "2.4.02"
 
 static const char description[] =
     I18N_NOOP("A periodic table of the elements");
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 #endif
 
     KAboutData about("kalzium", 0, ki18n("Kalzium"), version, ki18n(description),
-                     KAboutData::License_GPL, ki18n("(C) 2002-2011 Carsten Niehaus"), KLocalizedString(), "http://edu.kde.org/kalzium");
+                     KAboutData::License_GPL, ki18n("(C) 2002-2014 Carsten Niehaus"), KLocalizedString(), "http://edu.kde.org/kalzium");
     about.addAuthor( ki18n("Carsten Niehaus"), KLocalizedString(), "cniehaus@kde.org" );
     about.addCredit( ki18n("Pino Toscano"), ki18n("Large code contributions; resident guru helping the other developers"));
     about.addCredit( ki18n("Benoit Jacob"), ki18n( "Base work on the molecular viewer, mentored Marcus during his SoC" ));
